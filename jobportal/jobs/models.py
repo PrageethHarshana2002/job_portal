2abaa488f75ef86b17b130d39bf6ba34f3840c2c
prageeth <prageeth.har2002@gmail.com>
2025-04-11 11:06:04 +0530

    created models and added db.sqlite3 file

--- jobportal/jobs/models.py
+++ jobportal/jobs/models.py
@@ -2,27 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-# Create your models here.
-class Application(models.Model):
-    STATUS_CHOICES = [
-        ('AP', 'Applied'),
-        ('RV', 'Under Review'),
-        ('IT', 'Interview'),
-        ('RJ', 'Rejected'),
-        ('AC', 'Accepted'),
-    ]
+class Company(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    website = models.URLField(blank=True)
+    logo = models.ImageField(upload_to='company_logos/', blank=True)
 
-    job = models.ForeignKey(Job, on_delete=models.CASCADE)
-    applicant = models.ForeignKey(JobSeeker, on_delete=models.CASCADE)
-    applied_date = models.DateTimeField(default=timezone.now)
-    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='AP')
-    cover_letter = models.TextField()
-
-    class Meta:
-        unique_together = ('job', 'applicant')
-
-    def __str__(self):
-        return f"{self.applicant} application for {self.job}"
+    def _str_(self):
+        return self.name
 
 class JobSeeker(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -33,15 +21,7 @@ class JobSeeker(models.Model):
     def __str__(self):
         return self.user.get_full_name()
 
-class Company(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    website = models.URLField(blank=True)
-    logo = models.ImageField(upload_to='company_logos/', blank=True)
 
-    def _str_(self):
-        return self.name
 
 class Job(models.Model):
     JOB_TYPES = [
@@ -62,3 +42,25 @@ class Job(models.Model):
 
     def _str_(self):
         return f"{self.title} at {self.company.name}"
+
+
+class Application(models.Model):
+    STATUS_CHOICES = [
+        ('AP', 'Applied'),
+        ('RV', 'Under Review'),
+        ('IT', 'Interview'),
+        ('RJ', 'Rejected'),
+        ('AC', 'Accepted'),
+    ]
+
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    applicant = models.ForeignKey(JobSeeker, on_delete=models.CASCADE)
+    applied_date = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='AP')
+    cover_letter = models.TextField()
+
+    class Meta:
+        unique_together = ('job', 'applicant')
+
+    def __str__(self):
+        return f"{self.applicant} application for {self.job}"

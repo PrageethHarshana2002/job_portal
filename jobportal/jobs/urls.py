05c687d3dc0e0e23ed8045fa0a150c8782a0a5a5
maruchi <maruchinavodya422@gmail.com>
2025-04-16 19:35:13 +0530

    updated path for home page in urls.py file

--- jobportal/jobs/urls.py
+++ jobportal/jobs/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('job/<int:job_id>/', views.job_detail, name='job_detail'),
 

e4176b44e57b9baad1c25204c3f65aabb51d26b0
prageeth <prageeth.har2002@gmail.com>
2025-04-18 11:37:09 +0530

    created job posting form in forms.py

--- jobportal/jobs/forms.py
+++ jobportal/jobs/forms.py
@@ -11,3 +11,12 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+
+class JobForm(forms.ModelForm):
+    class Meta:
+        model = Job
+        fields = ['title', 'description', 'location', 'job_type', 'closing_date']
+        widgets = {
+            'closing_date': forms.DateInput(attrs={'type': 'date'}),
+        }
